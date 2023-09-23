bafe390b013802ddb5a44c5048f98492bc6fb77f
Junior Hirwa <iamhirwejr@gmail.com>
2023-09-23 23:52:36 +0200

    working with dynamic data and props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,41 @@
 export default function Card(props) {
+    /**
+     *              img={exp.img}
+                    rating={exp.rating}
+                    reviewCount={exp.reviewCount}
+                    country={exp.country}
+                    title={exp.title}
+                    price={exp.price}
+                    openSpots={exp.openSpots}
+                    location={exp.location}
+     */
     console.log(props)
+    let displayText
+    if(props.item.openSpots === 0) {
+        displayText="SOLD OUT"
+    } else if(props.item.location === 'online') {
+        displayText = "ONLINE"
+    }
     return (
         <>
             <article className='m-4 w-60'>
-                <div>
+                <div className="relative">
                     <div>
-                      <img src={`/images/${props.img}`} className='w-full'/>
+                      <img src={`/images/${props.item.img}`} className='w-full'/>
                     </div>
+                    {displayText && <div className="absolute bg-white p-1 top-2 left-2 text-sm font-semibold">{displayText}</div>}
                     <div className='py-4 px-2'>
                         <div className="flex gap-2 items-center">
                             <img src="/images/Star.svg" className='w-5'/>
                             <div className='flex gap-2'>
-                                <p>{props.rating}</p>
-                                <p className='font-light'>({props.reviewCount}).</p>
-                                <p className='font-light'>{props.country}</p>
+                                <p>{props.item.rating}</p>
+                                <p className='font-light'>({props.item.reviewCount}).</p>
+                                <p className='font-light'>{props.item.country}</p>
                             </div>
                         </div>
                        <div className='mt-1'>
-                        <p className='font-light text-sm'>{props.title}</p>
-                        <p className='font-bold'>From ${props.price}<span className='font-light'> /person</span></p>
+                        <p className='font-light text-sm'>{props.item.title}</p>
+                        <p className='font-bold'>From ${props.item.price}<span className='font-light'> /person</span></p>
                        </div>
                     </div>
                 </div>
